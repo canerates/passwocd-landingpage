80c92e77d99f8b963914d6c6a6357334c49d04ea
Caner Ates <cnrates@gmail.com>
2024-07-19 20:27:01 +0800

    Localization files

--- src/i18n.js
+++ src/i18n.js
@@ -3,8 +3,15 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
+import translations_de from './locales/de/translations.json'
 import translations_en from './locales/en/translations.json'
+import translations_es from './locales/es/translations.json'
+import translations_fr from './locales/fr/translations.json'
+import translations_ja from './locales/ja/translations.json'
+import translations_ko from './locales/ko/translations.json'
 import translations_tr from './locales/tr/translations.json'
+import translations_zhCN from './locales/zh-cn/translations.json'
+import translations_zhTW from './locales/zh-tw/translations.json'
 
 i18n
   .use(Backend)
@@ -14,14 +21,35 @@ i18n
     debug: true,
     fallbackLng: "en",
     resources: {
-      en: {
+      "de": {
+        translations: translations_de
+      },
+      "en": {
         translations: translations_en
       },
-      tr: {
+      "es": {
+        translations: translations_es
+      },
+      "fr": {
+        translations: translations_fr
+      },
+      "ja": {
+        translations: translations_ja
+      },
+      "ko": {
+        translations: translations_ko
+      },
+      "tr": {
         translations: translations_tr
-      }
+      },
+      "zh-CN": {
+        translations: translations_zhCN
+      },
+      "zh-TW": {
+        translations: translations_zhTW
+      },
     },
-    supportedLngs: ["en", "tr"],
+    supportedLngs: ["de", "en", "es", "fr", "ja", "ko", "tr", "zh-CN", "zh-TW"],
     ns: ['translations'],
     interpolation: {
       escapeValue: false,
